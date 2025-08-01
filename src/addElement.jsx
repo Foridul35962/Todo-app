0ca504e073d951e0c6eb4e1d9ece5da0384348de
Foridul35962 <foridulislam35962@gmail.com>
2025-08-01 18:44:10 +0600

    adding task done

--- src/addElement.jsx
+++ src/addElement.jsx
@@ -1,12 +1,22 @@
 const addElement = ({ viewTask }) => {
+    let isDone = false;
+    const taskDone =(event) =>{
+        const text = event.currentTarget.querySelector('p');
+        text.classList.toggle('line-through');
+        const icon = event.currentTarget.querySelector('i');
+        icon.classList.toggle('fa-circle-check');
+        icon.classList.toggle('fa-circle');
+        icon.classList.toggle('fa-solid');
+        icon.classList.toggle('fa-regular');
+    }
     return (
         <div className="w-full">
             {
-                viewTask.map((event, idx)=>(
+                viewTask.map((item, idx)=>(
                     <div key={idx} className="bg-gray-800 flex justify-between p-2 py-3 items-center my-2 w-full rounded-2xl text-white">
-                        <div className="flex items-center justify-center gap-3 text-cyan-400">
-                            <i className="fa-solid fa-circle-check"></i>
-                            <p className="text-xl text-white">{event}</p>
+                        <div className="flex items-center justify-center gap-3 text-cyan-400 overflow-hidden" onClick={taskDone}>
+                            <i class="fa-regular fa-circle"></i>
+                            <p className="text-xl text-white">{item}</p>
                         </div>
                         <i className="fa-solid fa-trash mr-2"></i>
                     </div>
@@ -15,5 +25,4 @@ const addElement = ({ viewTask }) => {
         </div>
     )
 }
-
 export default addElement;
