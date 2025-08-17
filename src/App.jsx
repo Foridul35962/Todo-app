22f8b3241b6a003dc4c75ebc1efa7546eabde324
Foridul35962 <foridulislam35962@gmail.com>
2025-08-17 16:34:15 +0600

    adding notification toastify

--- src/App.jsx
+++ src/App.jsx
@@ -2,40 +2,85 @@ import { useState } from "react";
 import Head from "./head";
 import InputValue from "./inputValue";
 import AddElement from "./addElement";
+import { ToastContainer, toast, Bounce } from 'react-toastify';
+
 function App() {
   let [task, newTask] = useState("");
   let [viewTask, updateViewTask] = useState([]);
 
-  const addInput = (event) =>{
+  const addInput = (event) => {
     newTask(event.target.value);
   }
 
-  const addElement = () =>{
-    if(task.trim()!==""){
-      updateViewTask([...viewTask,{text:task, done: false}]);
+  const addElement = () => {
+    if (task.trim() !== "") {
+      updateViewTask([...viewTask, { text: task, done: false }]);
       newTask("");
     }
   };
 
-  const toggleItem= (idx)=>{
-    const updated = viewTask.map((item,index)=>(
-      index===idx? {...item, done:!item.done} : item
+  const toggleItem = (idx) => {
+    const updated = viewTask.map((item, index) => (
+      index === idx ? { ...item, done: !item.done } : item
     ))
     updateViewTask(updated);
   };
 
-  const taskDelete = (idx)=>{
-    const newViewTask=viewTask.filter((_,index)=>index!==idx);
+  const taskDelete = (idx) => {
+    const newViewTask = viewTask.filter((_, index) => index !== idx);
     updateViewTask(newViewTask);
+  }
+
+  const addNotification = () => {
+    toast.success('The task has been successfully added!', {
+      position: "top-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: false,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "colored",
+      transition: Bounce,
+    });
+  }
+
+  const doneNotification = () => {
+    toast.info('The work has been completed', {
+      position: "top-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: false,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "colored",
+      transition: Bounce,
+    });
+  }
+
+  const deleteNotification = () => {
+    toast.error('The task has been completely deleted', {
+      position: "top-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: false,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "colored",
+      transition: Bounce,
+    });
   }
 
   return (
     <div className="flex items-center justify-center h-dvh w-full">
       <div className="flex flex-col items-center justify-center bg-black p-10 rounded-2xl w-md">
         <Head></Head>
-        <InputValue addInput={addInput} addElementClick={addElement} task={task}></InputValue>
-        <AddElement viewTask={viewTask} taskDelete={taskDelete} toggleItem={toggleItem}></AddElement>
+        <InputValue addInput={addInput} addElementClick={addElement} task={task} addNotification={addNotification}></InputValue>
+        <AddElement viewTask={viewTask} taskDelete={taskDelete} toggleItem={toggleItem} doneNotification={doneNotification} deleteNotification={deleteNotification}></AddElement>
       </div>
+      <ToastContainer />
     </div>
   )
 }
--- src/addElement.jsx
+++ src/addElement.jsx
@@ -1,14 +1,14 @@
-const addElement = ({ viewTask, taskDelete, toggleItem }) => {
+const addElement = ({ viewTask, taskDelete, toggleItem, doneNotification, deleteNotification }) => {
     return (
         <div className="w-full">
             {
                 viewTask.map((item, idx)=>(
                     <div key={idx} className="bg-gray-800 flex justify-between p-2 py-3 items-center my-2 w-full rounded-2xl text-white">
-                        <div className="flex items-center justify-center gap-3 text-cyan-400 overflow-hidden cursor-pointer" onClick={()=>toggleItem(idx)}>
+                        <div className="flex items-center justify-center gap-3 text-cyan-400 overflow-hidden cursor-pointer" onClick={()=>{toggleItem(idx); doneNotification();}}>
                             <i className={`${item.done? "fa-solid fa-circle-check": "fa-regular fa-circle"}`}></i>
                             <p className={`${item.done? 'line-through text-gray-400': 'text-white'} text-xl`}>{item.text}</p>
                         </div>
-                        <i className="fa-solid fa-trash mr-2 cursor-pointer" onClick={()=>taskDelete(idx)}></i>
+                        <i className="fa-solid fa-trash mr-2 cursor-pointer" onClick={()=>{taskDelete(idx); deleteNotification();}}></i>
                     </div>
                 ))
             }
